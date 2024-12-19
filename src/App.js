5ab849802a5c8e8f40fc48347706fc1a766c8ac2
ravindrasharmacs <ravindra.sharmacs@gmail.com>
2024-12-19 13:24:31 +0530

    react_008 : create a modal component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import React  , {useState } from "react";
+import React, { useState } from "react";
+import Modal from "./Modal";
 
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const [count, setCount] = useState(0);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
   const increment = () => {
     setCount(count + 1);
@@ -13,6 +22,12 @@ function App() {
 
   return (
     <div>
+      <h1>Modal App</h1>
+      <button onClick={openModal}>Open Modal</button>
+      <Modal isOpen={isModalOpen} onClose={closeModal}>
+        <h2>This is a Modal</h2>
+        <p>You can put any content here</p>
+      </Modal>
       <h1>Counter App</h1>
       <p>Count: {count}</p>
       <button onClick={increment}>Increment</button>
